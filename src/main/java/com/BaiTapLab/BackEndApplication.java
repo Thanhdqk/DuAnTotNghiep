b08ea9927709dce89a0ad02adb2226368890f02e
Thanhdqk <thanh02062004@gmail.com>
2024-09-24 16:13:57 +0700

    Bổ sung

--- src/main/java/com/BaiTapLab/BackEndApplication.java
+++ src/main/java/com/BaiTapLab/BackEndApplication.java
@@ -1,13 +1,11 @@
-package com.BaiTapLab.Config;
+package com.BaiTapLab;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BackEndApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(BackEndApplication.class, args);
 	}
-
 }
---
+++ src/main/java/com/BaiTapLab/Repository/Test.java
@@ -0,0 +1,5 @@
+package com.BaiTapLab.Repository;
+
+public class Test {
+
+}

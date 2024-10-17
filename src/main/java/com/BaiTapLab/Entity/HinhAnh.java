af71b029dd40768c542d9dd0f50c9f8e4582efd1
Thanhdqk <thanh02062004@gmail.com>
2024-10-17 23:09:52 +0700

    Sửa lại nữa

--- src/main/java/com/BaiTapLab/Entity/HinhAnh.java
+++ src/main/java/com/BaiTapLab/Entity/HinhAnh.java
@@ -8,6 +8,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
@@ -23,5 +24,6 @@ public class HinhAnh {
 	public String ten_hinh;
 	
 	@ManyToOne
+	@JoinColumn(name = "accountID")
 	public Users users;
 }

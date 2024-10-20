3152641a1050cedd2148f8cdf18f75147afccdcd
NTphat2004 <phongnguyen.0091@gmail.com>
2024-10-20 09:39:04 +0700

    demo load dữ liệu table bài đăng

---
+++ src/main/java/com/BaiTapLab/Controller/testbaidang.java
@@ -0,0 +1,29 @@
+package com.BaiTapLab.Controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.BaiTapLab.Entity.BaiDang;
+import com.BaiTapLab.Repository.BaiDangRepo;
+
+@RestController
+@CrossOrigin
+@RequestMapping("baidang")
+public class testbaidang {
+
+	@Autowired
+	BaiDangRepo baidangrepo;
+
+	@GetMapping("filltable")
+	public List<BaiDang> filltable() {
+		List<BaiDang> listbaidang = baidangrepo.findAll();
+		System.out.println(listbaidang.get(0));
+		return listbaidang;
+	}
+
+}
---
+++ src/main/java/com/BaiTapLab/Repository/BaiDangRepo.java
@@ -0,0 +1,7 @@
+package com.BaiTapLab.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.BaiTapLab.Entity.BaiDang;
+public interface BaiDangRepo extends JpaRepository <BaiDang,String> {
+
+}

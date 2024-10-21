13ba684f11dcb79fe080a476678ab0063392697f
Thanhdqk <thanh02062004@gmail.com>
2024-10-22 00:18:19 +0700

    Frontend của thành nè

--- src/index.js
+++ src/index.js
@@ -3,11 +3,25 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Kho from './components/Quanlykho';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Dashboard from './components/Dashboard';
+import Nhaphang from './components/Quanlynhaphang';
+import Sanpham from './components/Quanlysanpham';
+import Voucher from './components/Quanlyvoucher';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/kho" element={<Kho />} />
+        <Route path="/nhaphang" element={<Nhaphang />} />
+        <Route path="/Sanpham" element={<Sanpham />} />
+        <Route path="/Voucher" element={<Voucher />} />
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
 
